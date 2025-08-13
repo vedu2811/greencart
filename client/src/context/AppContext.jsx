cf5574b1ea45526b2a571560158e556b18f625a4
Vedant <vedant28j@gmail.com>
2025-08-13 18:34:53 +0000

    App Context jsx file

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+// createContext is a hook - function to create context object to share data across multiple components
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const value = {};
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
