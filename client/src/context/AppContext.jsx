5aa84070ebd891d21af1334335c823d2e7ffdd8a
Vedant <vedant28j@gmail.com>
2025-08-13 19:02:18 +0000

    importing useState hook from react-router-dom

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // createContext is a hook - function to create context object to share data across multiple components
