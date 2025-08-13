e51a083f3f4534bbbabca101feb00d52845d69ac
Vedant <vedant28j@gmail.com>
2025-08-13 21:13:34 +0000

    Adding Logout function to the logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -95,7 +95,10 @@ const Navbar = () => {
             Login
           </button>
         ) : (
-          <button className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+          <button
+            onClick={logout}
+            className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm"
+          >
             Login
           </button>
         )}
