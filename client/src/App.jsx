5d29d71fc6bb1411b715da84072e8539ba196eca
Vedant <vedant28j@gmail.com>
2025-08-13 21:59:19 +0000

    Mounted the home page file ot the app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
