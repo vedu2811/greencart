92e050f4138b41382ec95295b2ad92c64742a291
Vedant <vedant28j@gmail.com>
2025-08-13 19:26:23 +0000

    Updating navlinks at the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
 
       {/* Desktop Menu */}
       <div className="hidden sm:flex items-center gap-8">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/products"}>All Product</NavLink>
+        <NavLink to={"/"}>Contact</NavLink>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
