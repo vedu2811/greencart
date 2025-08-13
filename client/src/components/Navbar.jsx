2f6ce8175478dfb1c9cf6356547802aaabb7fb3a
Vedant <vedant28j@gmail.com>
2025-08-13 21:41:51 +0000

    Adding onclick to the cart icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,7 +37,10 @@ const Navbar = () => {
           <img src={assets.search_icon} alt="search" className="w-4 h-4" />
         </div>
 
-        <div className="relative cursor-pointer">
+        <div
+          onClick={() => navigate("/cart")}
+          className="relative cursor-pointer"
+        >
           <img src={assets.cart_icon} alt="cart" className="w-6 opacity-80" />
           <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
             3
@@ -95,7 +98,7 @@ const Navbar = () => {
             All Product
           </NavLink>
           {user && (
-            <NavLink to="/" onCLick={() => setOpen(false)}>
+            <NavLink to="/my-orders" onCLick={() => setOpen(false)}>
               My Orders
             </NavLink>
           )}
