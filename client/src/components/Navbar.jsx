bf41bc5de15cd8440ede4b777984c3b304a3fe03
Vedant <vedant28j@gmail.com>
2025-08-13 21:21:30 +0000

    Typo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -100,7 +100,7 @@ const Navbar = () => {
               onClick={logout}
               className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm"
             >
-              Login
+              Logout
             </button>
           )}
         </div>
