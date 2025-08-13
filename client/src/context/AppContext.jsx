3132cf46acf0f7aed4f71cc76083e918ceb11ac4
Vedant <vedant28j@gmail.com>
2025-08-13 21:10:22 +0000

    Added showUserLogin and its setter using useState()

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,8 +8,17 @@ export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
 
-  const value = { navigate, user, setUser, isSeller, setIsSeller };
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
