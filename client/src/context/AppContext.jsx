59b05ad9305aa55a2d3589fab662f2809aad4f5c
Vedant <vedant28j@gmail.com>
2025-08-13 18:36:53 +0000

    Adding hooks to the value object

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,7 +5,11 @@ import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+
+  const value = { navigate, user, setUser, isSeller, setIsSeller };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
