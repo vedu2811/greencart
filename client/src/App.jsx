edf2e417b233ca2a1f9554b93fd143bab41131a1
Vedant <vedant28j@gmail.com>
2025-08-13 22:04:11 +0000

    Adding css to the image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <div>
+      <div className="px-6 md:px-16 lg:px-24 xl:px-32">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
