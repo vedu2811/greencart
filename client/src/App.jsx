b13a34004445cea48985dc81cbf06890076f2789
Vedant <vedant28j@gmail.com>
2025-08-13 22:15:08 +0000

    isSellerPath is a boolean variable which determines whether the user is a customer or seller: used useLocation() hook which has a pathname in it storing routes, we are showing navbar and Home page css only to the user right now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 const App = () => {
+  const isSellerPath = useLocation().pathname.includes("seller");
+
   return (
     <div>
-      <Navbar />
-      <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+      {isSellerPath ? null : <Navbar />}
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
