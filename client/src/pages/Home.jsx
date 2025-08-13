7abe4278f2fb5373364a9245c40fa5b621adadd6
Vedant <vedant28j@gmail.com>
2025-08-13 21:58:50 +0000

    Home Page file: mounted the mainbanner here

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Mainbanner from "../components/Mainbanner";
+
+const Home = () => {
+  return (
+    <div className="mt-10">
+      <Mainbanner />
+    </div>
+  );
+};
+
+export default Home;
