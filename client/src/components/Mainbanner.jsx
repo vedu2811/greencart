e83fea4e839124298b5aa8668180ee5dbcd4c0b7
Vedant <vedant28j@gmail.com>
2025-08-13 23:19:46 +0000

    Adding links into the same div as the h1, this leads all these items on the main banner image because of inset-0

--- client/src/components/Mainbanner.jsx
+++ client/src/components/Mainbanner.jsx
@@ -19,31 +19,32 @@ const Mainbanner = () => {
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">
           Freshness You Can Trust, Savings You will Love!{" "}
         </h1>
-      </div>
-      <div className="flex items-center mt-6 font-medium">
-        <Link
-          to={"/products"}
-          className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover-bg-primary-dull transition rounded text-white cursor-pointer"
-        >
-          Shop Now
-          <img
-            className="transition group-focus:translate-x-1"
-            src={assets.white_arrow_icon}
-            alt="arrow"
-          />
-        </Link>
 
-        <Link
-          to={"/products"}
-          className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer"
-        >
-          Explore Deals
-          <img
-            className="transition group-hover:translate-x-1"
-            src={assets.black_arrow_icon}
-            alt="arrow"
-          />
-        </Link>
+        <div className="flex items-center mt-6 font-medium">
+          <Link
+            to={"/products"}
+            className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover-bg-primary-dull transition rounded text-white cursor-pointer"
+          >
+            Shop Now
+            <img
+              className="md:hidden transition group-focus:translate-x-1"
+              src={assets.white_arrow_icon}
+              alt="arrow"
+            />
+          </Link>
+
+          <Link
+            to={"/products"}
+            className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer"
+          >
+            Explore Deals
+            <img
+              className="transition group-hover:translate-x-1"
+              src={assets.black_arrow_icon}
+              alt="arrow"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
