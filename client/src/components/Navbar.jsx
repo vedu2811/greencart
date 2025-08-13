9ef41febdb24cd5c76e734579f7b42d7d63cd255
Vedant <vedant28j@gmail.com>
2025-08-13 19:30:14 +0000

    Changing the color of the navbar to primary

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -64,12 +64,12 @@ const Navbar = () => {
               stroke-linejoin="round"
             />
           </svg>
-          <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full">
+          <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
             3
           </button>
         </div>
 
-        <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+        <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
           Login
         </button>
       </div>
@@ -108,7 +108,7 @@ const Navbar = () => {
         <a href="#" className="block">
           Contact
         </a>
-        <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+        <button className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
           Login
         </button>
       </div>
