1491bc76fb083aaa349708aa4e521f96c1ff901b
Vedant <vedant28j@gmail.com>
2025-08-14 16:14:58 +0000

    adding product prop to the product card in the best seller file

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import { useAppContext } from "../context/AppContext";
 
 const BestSeller = () => {
+  const { products } = useAppContext();
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
       <div>
-        <ProductCard />
+        <ProductCard product={products[0]} />
       </div>
     </div>
   );
