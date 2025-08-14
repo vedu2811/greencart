3a963f7b79f75482cb854aec3ebb7278b1de50a0
Vedant <vedant28j@gmail.com>
2025-08-14 00:45:08 +0000

    Mounting best seller page to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Mainbanner from "../components/Mainbanner";
 import Categories from "../components/Categories";
+import BestSeller from "../components/BestSeller";
 
 const Home = () => {
   return (
     <div className="mt-10">
       <Mainbanner />
       <Categories />
+      <BestSeller />
     </div>
   );
 };
