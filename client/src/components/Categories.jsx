0508dd82c9c0480eccd6f305a9f3e2043201ee91
Vedant <vedant28j@gmail.com>
2025-08-14 00:15:54 +0000

    Adding css to the Category cards

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -3,12 +3,16 @@ import { assets } from "../assets/assets";
 
 const Categories = () => {
   return (
-    <div>
-      <p>Categories</p>
-      <div>
-        <div>
-          <img src={assets.box_icon} alt="" />
-          <p>fruit</p>
+    <div className="mt-16">
+      <p className="text-2xl md:text-3xl font-medium">Categories</p>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6">
+        <div className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center">
+          <img
+            src={assets.box_icon}
+            alt=""
+            className="group-hover:scale-108 transition max-w-28"
+          />
+          <p className="text-sm font-medium">fruit</p>
         </div>
       </div>
     </div>
