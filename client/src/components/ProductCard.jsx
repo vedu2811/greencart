4b8ff2154fb0465fc71c89549a5c1a6c183d953d
Vedant <vedant28j@gmail.com>
2025-08-14 16:30:49 +0000

    Adding primary colors to the product card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -42,23 +42,23 @@ const ProductCard = ({ product }) => {
             <p>(4)</p>
           </div>
           <div className="flex items-end justify-between mt-3">
-            <p className="md:text-xl text-base font-medium text-indigo-500">
+            <p className="md:text-xl text-base font-medium text-primary">
               {currency}${product.offerPrice}{" "}
               <span className="text-gray-500/60 md:text-sm text-xs line-through">
                 {currency}${product.price}
               </span>
             </p>
-            <div className="text-indigo-500">
+            <div className="text-primary">
               {!cartItems[product._id] ? (
                 <button
-                  className="flex items-center justify-center gap-1 bg-indigo-100 border border-indigo-300 md:w-[80px] w-[64px] h-[34px] rounded text-indigo-600 font-medium"
+                  className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded"
                   onClick={() => setCount(1)}
                 >
                   <img src={assets.cart_icon} alt="cart_icon" />
                   Add
                 </button>
               ) : (
-                <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-indigo-500/25 rounded select-none">
+                <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-primary/25 rounded select-none">
                   <button
                     onClick={() => setCount((prev) => Math.max(prev - 1, 0))}
                     className="cursor-pointer text-md px-2 h-full"
