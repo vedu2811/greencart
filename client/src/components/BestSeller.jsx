0b56de409cb01887a9a6c7783cbcca2ca8e3b25f
Vedant <vedant28j@gmail.com>
2025-08-14 00:51:40 +0000

    Mounting Product Card into Best Seller

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ProductCard from "./ProductCard";
 
 const BestSeller = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div></div>
+      <div>
+        <ProductCard />
+      </div>
     </div>
   );
 };
