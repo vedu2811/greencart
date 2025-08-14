350a77f0b2a9910a462a6884851900650a9c5f13
Vedant <vedant28j@gmail.com>
2025-08-14 22:17:23 +0000

    Making all the small modifications to the footer file

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,19 @@
+import { assets, footerLinks } from "../assets/assets";
+
 const Footer = () => {
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
       <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
         <div>
-          <img
-            className="w-34 md:w-32"
-            src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg"
-            alt="dummyLogoColored"
-          />
+          <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
           <p className="max-w-[410px] mt-6">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde
-            quaerat eveniet cumque accusamus atque qui error quo enim fugiat?
+            We deliver fresh groceries and snacks straight to your door. Trusted
+            by thousands, we aim to make your shopping experience simple and
+            affordable.
           </p>
         </div>
         <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
-          {linkSections.map((section, index) => (
+          {footerLinks.map((section, index) => (
             <div key={index}>
               <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
                 {section.title}
@@ -22,8 +21,8 @@ const Footer = () => {
               <ul className="text-sm space-y-1">
                 {section.links.map((link, i) => (
                   <li key={i}>
-                    <a href="#" className="hover:underline transition">
-                      {link}
+                    <a href={link.url} className="hover:underline transition">
+                      {link.text}
                     </a>
                   </li>
                 ))}
@@ -33,8 +32,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-        Copyright 2025 © <a href="https://prebuiltui.com">PrebuiltUI</a> All
-        Right Reserved.
+        Copyright {new Date().getFullYear()} © Vedant Jalan All Right Reserved.
       </p>
     </div>
   );
