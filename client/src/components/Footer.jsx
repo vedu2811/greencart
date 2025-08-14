d1d29e1492d7ca8108f6b4dd946b81e6cec83d03
Vedant <vedant28j@gmail.com>
2025-08-14 22:20:05 +0000

    Adding link to the footer logo to the home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
       <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
         <div>
-          <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
+          <a href="#">
+            <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
+          </a>
           <p className="max-w-[410px] mt-6">
             We deliver fresh groceries and snacks straight to your door. Trusted
             by thousands, we aim to make your shopping experience simple and
