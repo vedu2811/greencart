073cc229aada031d1099885a8bae6d6703bf4251
Vedant <vedant28j@gmail.com>
2025-08-14 21:58:58 +0000

    Adding the margins and padding for the newsletter

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -1,6 +1,6 @@
 const NewsLetter = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center space-y-2">
+    <div className="flex flex-col items-center justify-center text-center space-y-2 mt-24 pb-14">
       <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</h1>
       <p className="md:text-lg text-gray-500/70 pb-8">
         Subscribe to get the latest offers, new arrivals, and exclusive
