031e4d7cc9cdb8ec4d2127ebe16d6cbdbc559f2e
Vedant <vedant28j@gmail.com>
2025-08-14 21:49:45 +0000

    typo errors and div element addition

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -3,7 +3,7 @@ import { assets, features } from "../assets/assets";
 
 const BottomBanner = () => {
   return (
-    <div className="realtive mt-24">
+    <div className="relative mt-24">
       <img
         src={assets.bottom_banner_image}
         alt="banner"
@@ -22,16 +22,18 @@ const BottomBanner = () => {
           {features.map((feature, index) => (
             <div key={index} className="flex items-center gap-4 mt-2">
               <img
-                rec={feature.icon}
+                src={feature.icon}
                 alt={feature.title}
                 className="md:w-11 w-9"
               />
-              <h3 className="text-lg md:text-xl font-semibold">
-                {feature.title}
-              </h3>
-              <p className="text-gray-500/70 text-xs md:text-sm">
-                {feature.description}
-              </p>
+              <div>
+                <h3 className="text-lg md:text-xl font-semibold">
+                  {feature.title}
+                </h3>
+                <p className="text-gray-500/70 text-xs md:text-sm">
+                  {feature.description}
+                </p>
+              </div>
             </div>
           ))}
         </div>
