00416441b1c01c0b7f846746c504fdcb1b49e944
Vedant <vedant28j@gmail.com>
2025-08-14 00:10:23 +0000

    Mounting categories to the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Mainbanner from "../components/Mainbanner";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <div className="mt-10">
       <Mainbanner />
+      <Categories />
     </div>
   );
 };
