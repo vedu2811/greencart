678172efd0ee1c947c7de1f77bc3364858088371
Vedant <vedant28j@gmail.com>
2025-08-14 00:43:05 +0000

    best seller file

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BestSeller = () => {
+  return (
+    <div className="mt-16">
+      <p>Best Sellers</p>
+      <div></div>
+    </div>
+  );
+};
+
+export default BestSeller;
