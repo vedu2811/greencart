572083b8de0d7c839fc3e67fd9fdeb25295e3e6e
Vedant <vedant28j@gmail.com>
2025-08-14 00:31:08 +0000

    Adding Categories array from the assets file

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { assets, categories } from "../assets/assets";
 
 const Categories = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Categories</p>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6">
-        <div className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center">
-          <img
-            src={assets.box_icon}
-            alt=""
-            className="group-hover:scale-108 transition max-w-28"
-          />
-          <p className="text-sm font-medium">fruit</p>
-        </div>
+        {categories.map((category, index) => {
+          <div
+            key={index}
+            className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center"
+          >
+            <img
+              src={category.image}
+              alt={category.text}
+              className="group-hover:scale-108 transition max-w-28"
+            />
+            <p className="text-sm font-medium">{category.text}</p>
+          </div>;
+        })}
       </div>
     </div>
   );
