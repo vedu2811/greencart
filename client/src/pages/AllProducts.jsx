0e1d7962f9789abcd9738caa5bcddcb0c9e7dab0
Vedant <vedant28j@gmail.com>
2025-08-14 23:36:50 +0000

    Adding css properties to the related divs

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -6,8 +6,9 @@ const AllProducts = () => {
 
   return (
     <div className="mt-16 flex flex-col">
-      <div>
-        <p>All products</p>
+      <div className="flex flex-col items-end w-max">
+        <p className="text-2xl font-medium uppercase">All products</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
       </div>
     </div>
   );
