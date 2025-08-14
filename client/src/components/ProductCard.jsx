cac7193d2a96a9425b5e6e8430d58da19db70610
Vedant <vedant28j@gmail.com>
2025-08-14 16:10:43 +0000

    importing all the cart related functions to the product card file

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,7 +4,14 @@ import { useAppContext } from "../context/AppContext";
 
 const ProductCard = ({ product }) => {
   const [count, setCount] = React.useState(0);
-  const { currency } = useAppContext();
+  const {
+    currency,
+    addToCart,
+    removeFromCart,
+    cartItems,
+    navigate,
+    uodateCartItem,
+  } = useAppContext();
 
   return (
     <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
