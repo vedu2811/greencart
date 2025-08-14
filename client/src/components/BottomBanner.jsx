f6283e6d5cd411cd368e67f43e654697628906ba
Vedant <vedant28j@gmail.com>
2025-08-14 21:31:13 +0000

    Adding all the contents of the banner to the file

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { assets, features } from "../assets/assets";
 
 const BottomBanner = () => {
   return (
@@ -14,11 +14,26 @@ const BottomBanner = () => {
         alt="banner"
         className="w-full md:hidden"
       />
-      <div>
+      <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
         <div>
           <h1 className="text-2xl md:text-3xl font-semibold text-primary mb-6">
             Why We Are the Best?
           </h1>
+          {features.map((feature, index) => (
+            <div key={index} className="flex items-center gap-4 mt-2">
+              <img
+                rec={feature.icon}
+                alt={feature.title}
+                className="md:w-11 w-9"
+              />
+              <h3 className="text-lg md:text-xl font-semibold">
+                {feature.title}
+              </h3>
+              <p className="text-gray-500/70 text-xs md:text-sm">
+                {feature.description}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
