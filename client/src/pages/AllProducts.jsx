67fd1b668a2384ea72170fc96572ef3117ffa092
Vedant <vedant28j@gmail.com>
2025-08-14 23:35:15 +0000

    Importing products array from context file

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useAppContext } from "../context/AppContext";
 
 const AllProducts = () => {
+  const { products } = useAppContext();
+
   return (
     <div className="mt-16 flex flex-col">
       <div>
