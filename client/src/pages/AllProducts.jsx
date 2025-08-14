3fc08d60dff86735732cca237b2edb576e19a750
Vedant <vedant28j@gmail.com>
2025-08-14 23:34:37 +0000

    All products file

---
+++ client/src/pages/AllProducts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AllProducts = () => {
+  return (
+    <div className="mt-16 flex flex-col">
+      <div>
+        <p>All products</p>
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
