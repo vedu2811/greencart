515760e8131c5bd3e52d7e513ef0389ba31e3104
Vedant <vedant28j@gmail.com>
2025-08-15 16:25:14 +0000

    Adding click feature to the shopping button

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -109,7 +109,13 @@ const Cart = () => {
           </div>
         ))}
 
-        <button className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium">
+        <button
+          onClick={() => {
+            navigate("/products");
+            scrollTo(0, 0);
+          }}
+          className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium"
+        >
           <img
             src={assets.arrow_right_icon_colored}
             alt="arrow"
