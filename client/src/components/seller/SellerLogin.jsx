4ca9bd07ffdd1f63affe1b1048fc6acdf6e23636
Vedant <vedant28j@gmail.com>
2025-08-15 23:48:03 +0000

    Use states

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 
 const SellerLogin = () => {
   const [isSeller, setIsSeller, navigate] = useAppContext;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return <div>SellerLogin</div>;
 };
