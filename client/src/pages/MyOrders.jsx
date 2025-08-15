2d9adab1d976dfd6f65bdd0eea2a1f1416b7311b
Vedant <vedant28j@gmail.com>
2025-08-15 22:12:42 +0000

    Adding all the required css for the order card

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -34,7 +34,12 @@ const MyOrders = () => {
             </span>
           </p>
           {order.items.map((item, index) => (
-            <div>
+            <div
+              key={index}
+              className={`relative bg-white text-gray-500/70 ${
+                order.items.length !== index + 1 && "border-b"
+              } border-gray-300 flex flex-col md:flex-row md:items-center justify-between p-4 py-5 md:gap-16 w-full max-w-4xl`}
+            >
               <div className="flex items-center mb-4 md:mb-0">
                 <div className="bg-primary/10 p-4 rounded-lg">
                   <img
@@ -42,23 +47,23 @@ const MyOrders = () => {
                     alt="product-image"
                     className="w-16 h-16"
                   />
-                  <div className="ml-4">
-                    <h2 className="text-4xl font-medium text-gray-800">
-                      {item.product.name}
-                    </h2>
-                    <p>Category: {item.product.category}</p>
-                  </div>
                 </div>
-                <div>
-                  <p>Quantity: {item.quantity || "1"}</p>
-                  <p>Status: {order.status}</p>
-                  <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+                <div className="ml-4">
+                  <h2 className="text-xl font-medium text-gray-800">
+                    {item.product.name}
+                  </h2>
+                  <p>Category: {item.product.category}</p>
                 </div>
-                <p className="text-primary text-lg font-medium">
-                  Amount: {currency}
-                  {item.product.offerPrice * item.quantity}
-                </p>
               </div>
+              <div className="text-primary text-lg font-medium">
+                <p>Quantity: {item.quantity || "1"}</p>
+                <p>Status: {order.status}</p>
+                <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+              </div>
+              <p className="text-primary text-lg font-medium">
+                Amount: {currency}
+                {item.product.offerPrice * item.quantity}
+              </p>
             </div>
           ))}
         </div>
