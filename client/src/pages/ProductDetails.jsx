c144a36eabd9b2159c36b5e54e0a187190f6ba6e
Vedant <vedant28j@gmail.com>
2025-08-15 13:54:33 +0000

    navigation to products page on clicking the button

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -130,7 +130,13 @@ const ProductDetails = () => {
                 <ProductCard key={index} product={product} />
               ))}
           </div>
-          <button className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition">
+          <button
+            onClick={() => {
+              navigate("/products");
+              scrollTo(0, 0);
+            }}
+            className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition"
+          >
             See more
           </button>
         </div>
