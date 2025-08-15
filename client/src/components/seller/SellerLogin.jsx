6a8ddeb2aeb4644b77edecd483d4f05883a73a30
Vedant <vedant28j@gmail.com>
2025-08-15 23:59:26 +0000

    Css to the login form div

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -23,7 +23,7 @@ const SellerLogin = () => {
         onSubmit={onSubmitHandler}
         className="min-h-screen flex items-center text-sm text-gray-600"
       >
-        <div>
+        <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200">
           <p className="text-2xl font-medium m-auto">
             <span className="text-primary">Seller</span> Login
           </p>
