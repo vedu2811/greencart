56590536bfc894445cac3d108ed6d8ec70d7b03d
Vedant <vedant28j@gmail.com>
2025-08-15 13:44:20 +0000

    Adding Related products below the product details

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -115,6 +115,14 @@ const ProductDetails = () => {
             </div>
           </div>
         </div>
+
+        {/* Related Products  */}
+        <div className="flex flex-col items-center mt-20">
+          <div className="flec flex-col items-center w-max">
+            <p className="text-3xl font-medium">Related Products</p>
+            <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
+          </div>
+        </div>
       </div>
     )
   );
