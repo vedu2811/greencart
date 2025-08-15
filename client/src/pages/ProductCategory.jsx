1b6e23bd0e02cc43ac71c1a3d60ff326e7d700b8
Vedant <vedant28j@gmail.com>
2025-08-15 01:54:29 +0000

    Adding css to the product categories

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -16,10 +16,13 @@ const ProductCategory = () => {
   );
 
   return (
-    <div>
+    <div className="mt-16">
       {searchCategory && (
-        <div>
-          <p>{searchCategory.text.toUpperCase()}</p>
+        <div className="flex flex-col items-end w-max">
+          <p className="text-2xl font-medium">
+            {searchCategory.text.toUpperCase()}
+          </p>
+          <div className="w-16 h-0.5 bg-primary rounded-full"></div>
         </div>
       )}
     </div>
