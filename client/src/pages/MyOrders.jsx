c016b483176373a225d91374e4d19fc355c5714f
Vedant <vedant28j@gmail.com>
2025-08-15 21:09:26 +0000

    Adding css to the my order divs

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -21,8 +21,11 @@ const MyOrders = () => {
         <div className="w-16 h-0.5 bg-primary rounded-full"></div>
       </div>
       {myOrders.map((order, index) => (
-        <div>
-          <p>
+        <div
+          key={index}
+          className="border border-gray-300 rounded-lg mb-10 p-4 py-5 max-w-4xl"
+        >
+          <p className="flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col">
             <span>OrderId : {order._id}</span>
             <span>Payment : {order.paymentType}</span>
             <span>
