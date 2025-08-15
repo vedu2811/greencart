ef6aff41aed1e9f9becdc2ed3287aa431349727c
Vedant <vedant28j@gmail.com>
2025-08-15 19:32:36 +0000

    Add Address file

---
+++ client/src/pages/AddAddress.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Input Field Component
+const inputField = () => <input type="text" />;
+
+const AddAddress = () => {
+  return (
+    <div>
+      <p>
+        Add Shipping <span>Address</span>
+      </p>
+    </div>
+  );
+};
+
+export default AddAddress;
