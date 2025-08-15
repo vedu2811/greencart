2c188e629bfcddff39ea5f3d628d53054ef43a58
Vedant <vedant28j@gmail.com>
2025-08-15 23:49:35 +0000

    use Effect hook to navigate the user to the seller page

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 
 const SellerLogin = () => {
   const [isSeller, setIsSeller, navigate] = useAppContext;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (isSeller) {
+      navigate("/seller");
+    }
+  }, [isSeller]);
 
   return <div>SellerLogin</div>;
 };
