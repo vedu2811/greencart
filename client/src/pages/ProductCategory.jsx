824141bc0465b275fd370ecb5ed98e2478d165b8
Vedant <vedant28j@gmail.com>
2025-08-15 01:51:09 +0000

    Search category and filtered products variables

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { useAppContext } from "../context/AppContext";
 import { useParams } from "react-router-dom";
+import { categories } from "../assets/assets";
 
 const ProductCategory = () => {
   const { products } = useAppContext();
   const { category } = useParams();
+
+  const searchCategory = categories.find(
+    (item) => item.path.toLowerCase() === category
+  );
+
+  const filteredProducts = products.filter(
+    (product) => product.category.toLowerCase() === category
+  );
 
   return <div></div>;
 };
