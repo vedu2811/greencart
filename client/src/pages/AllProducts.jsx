322959b4427af0694e99bdf96844d4571ca9b769
Vedant <vedant28j@gmail.com>
2025-08-15 01:19:42 +0000

    added css for all the product cards

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -25,7 +25,7 @@ const AllProducts = () => {
         <div className="w-16 h-0.5 bg-primary rounded-full"></div>
       </div>
 
-      <div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
         {filterProducts
           .filter((product) => product.inStock)
           .map((product, index) => (
