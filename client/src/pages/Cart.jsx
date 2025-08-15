d3d50c6baee1ef27a4cfd18996db7da5a3b71dd8
Vedant <vedant28j@gmail.com>
2025-08-15 17:15:54 +0000

    Changing color to primary

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -43,7 +43,7 @@ const Cart = () => {
       <div className="flex-1 max-w-4xl">
         <h1 className="text-3xl font-medium mb-6">
           Shopping Cart{" "}
-          <span className="text-sm text-indigo-500">{getCartCount()}</span>
+          <span className="text-sm text-primary">{getCartCount()}</span>
         </h1>
 
         <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
@@ -122,7 +122,7 @@ const Cart = () => {
             navigate("/products");
             scrollTo(0, 0);
           }}
-          className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium"
+          className="group cursor-pointer flex items-center mt-8 gap-2 text-primary font-medium"
         >
           <img
             src={assets.arrow_right_icon_colored}
@@ -147,7 +147,7 @@ const Cart = () => {
             </p>
             <button
               onClick={() => setShowAddress(!showAddress)}
-              className="text-indigo-500 hover:underline cursor-pointer"
+              className="text-primary hover:underline cursor-pointer"
             >
               Change
             </button>
@@ -167,7 +167,7 @@ const Cart = () => {
                 ))}
                 <p
                   onClick={() => navigate("/add-address")}
-                  className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10"
+                  className="text-primary text-center cursor-pointer p-2 hover:bg-primary/10"
                 >
                   Add address
                 </p>
@@ -218,7 +218,7 @@ const Cart = () => {
 
         <button
           onClick={placeOrder}
-          className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition"
+          className="w-full py-3 mt-6 cursor-pointer bg-primary text-white font-medium hover:bg-primary-dull transition"
         >
           {paymentOption === "COD" ? "Place Order" : "Proceed to Checkout"}
         </button>
