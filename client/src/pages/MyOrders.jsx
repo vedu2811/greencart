ef0a0fc525491e13557611f61af0fae65b9eb70a
Vedant <vedant28j@gmail.com>
2025-08-15 20:51:46 +0000

    adding css to the my orders page

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const MyOrders = () => {
   return (
-    <div>
-      <p>My Orders</p>
+    <div className="mt-16 pb-16">
+      <div className="flex flex-col items-end w-max mb-8">
+        <p className="text-2xl font-medium uppercase">My Orders</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+      </div>
     </div>
   );
 };
