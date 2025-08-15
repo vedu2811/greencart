6415999d89c170ae57e0ca52c2aea5c00007dedc
Vedant <vedant28j@gmail.com>
2025-08-15 12:50:38 +0000

    importing states from app context

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -1,26 +1,13 @@
 import { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  const product = {
-    name: "Nike Pegasus 41 shoes",
-    category: "Sports",
-    price: 189,
-    offerPrice: 159,
-    rating: 4,
-    images: [
-      "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage.png",
-      "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage2.png",
-      "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage3.png",
-      "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage4.png",
-    ],
-    description: [
-      "High-quality material",
-      "Comfortable for everyday use",
-      "Available in different sizes",
-    ],
-  };
+  const { products, navigate, currency, addToCart } = useAppContext();
+  const { id } = useParams();
 
-  const [thumbnail, setThumbnail] = useState(product.images[0]);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [relatedProducts, setRelatedProducts] = useState(product.images[0]);
 
   return (
     product && (
