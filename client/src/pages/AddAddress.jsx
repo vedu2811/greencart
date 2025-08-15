5846944d678ff819d35dabb6cd292bf8cd94219e
Vedant <vedant28j@gmail.com>
2025-08-15 19:41:38 +0000

    Adding image and form to the add address page

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 // Input Field Component
 const inputField = () => <input type="text" />;
@@ -9,6 +10,16 @@ const AddAddress = () => {
       <p className="text-2xl md:text-3xl text-gray-500">
         Add Shipping <span className="font-semibold text-primary">Address</span>
       </p>
+      <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+        <div className="flex-1 max-w-md">
+          <form onSubmit={onSubmitHandler}></form>
+        </div>
+        <img
+          src={assets.add_address_iamge}
+          alt="address"
+          className="md:mr-16 mb-16 md:mt-0"
+        />
+      </div>
     </div>
   );
 };
