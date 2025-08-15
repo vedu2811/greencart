6a82542e2d40903af5d80a72eac9cd1aacdc95dd
Vedant <vedant28j@gmail.com>
2025-08-15 20:54:30 +0000

    adding states and function to add dummy orders in the page

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { dummyOrders } from "../assets/assets";
 
 const MyOrders = () => {
+  const [myOrders, setMyOrders] = useState([]);
+  const { currency } = useAppContext();
+
+  const fetchMyOrders = async () => {
+    setMyOrders(dummyOrders);
+  };
+
   return (
     <div className="mt-16 pb-16">
       <div className="flex flex-col items-end w-max mb-8">
