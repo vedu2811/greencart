2538cb84d6eecceb0c13c31f914645cdb3dad169
Vedant <vedant28j@gmail.com>
2025-08-15 20:46:12 +0000

    footer copyright typo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-        Copyright {new Date().getFullYear()} © Vedant Jalan All Right Reserved.
+        Copyright {new Date().getFullYear()} © Vedant Jalan. All Rights Reserved
       </p>
     </div>
   );
