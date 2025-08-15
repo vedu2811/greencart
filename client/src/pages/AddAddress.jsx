4bddee61e271eb361b4bb562d67a197308641c31
Vedant <vedant28j@gmail.com>
2025-08-15 19:49:02 +0000

    Css classes to the input field

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -4,6 +4,7 @@ import { assets } from "../assets/assets";
 // Input Field Component
 const inputField = ({ type, placeholder, name, handleChange, address }) => (
   <input
+    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outlie-none text-gray-500 focus:border-primary transition"
     type={type}
     placeholder={placeholder}
     onChange={handleChange}
