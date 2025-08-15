e1957b0a444754838a45bda7a6ef2a6afa55b3ee
Vedant <vedant28j@gmail.com>
2025-08-15 16:08:02 +0000

    returning only if products length >0 and cartitems exist

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -36,8 +36,8 @@ const Cart = () => {
     }
   }, [products, cartItems]);
 
-  return (
-    <div className="flex flex-col md:flex-row py-16 max-w-6xl w-full px-6 mx-auto">
+  return products.length > 0 && cartItems ? (
+    <div className="flex flex-col md:flex-row mt-16">
       <div className="flex-1 max-w-4xl">
         <h1 className="text-3xl font-medium mb-6">
           Shopping Cart <span className="text-sm text-indigo-500">3 Items</span>
@@ -192,7 +192,7 @@ const Cart = () => {
         </button>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Cart;
