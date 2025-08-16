e380639425d740b90d865baf9ef66c885e514c0e
Vedant <vedant28j@gmail.com>
2025-08-16 03:02:31 +0000

    Making small changes to the code

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -20,11 +20,15 @@ const ProductList = () => {
               </tr>
             </thead>
             <tbody className="text-sm text-gray-500">
-              {products.map((product, index) => (
-                <tr key={index} className="border-t border-gray-500/20">
+              {products.map((product) => (
+                <tr key={product._id} className="border-t border-gray-500/20">
                   <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate">
                     <div className="border border-gray-300 rounded overflow-hidden">
-                      <img src={product.image} alt="Product" className="w-16" />
+                      <img
+                        src={product.image[0]}
+                        alt="Product"
+                        className="w-16"
+                      />
                     </div>
                     <span className="truncate max-sm:hidden w-full">
                       {product.name}
@@ -32,15 +36,12 @@ const ProductList = () => {
                   </td>
                   <td className="px-4 py-3">{product.category}</td>
                   <td className="px-4 py-3 max-sm:hidden">
-                    ${product.offerPrice}
+                    {currency}
+                    {product.offerPrice}
                   </td>
                   <td className="px-4 py-3">
                     <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
-                      <input
-                        type="checkbox"
-                        className="sr-only peer"
-                        defaultChecked={product.inStock}
-                      />
+                      <input type="checkbox" className="sr-only peer" />
                       <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
                       <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
                     </label>
