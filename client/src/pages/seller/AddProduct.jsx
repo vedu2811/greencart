83a13cf427ebde967be66d2a981f1d5542837be4
Vedant <vedant28j@gmail.com>
2025-08-16 02:28:06 +0000

    use States

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const AddProduct = () => {
+  const [files, setFiles] = useState([]);
+  const [name, setName] = useState([]);
+  const [description, setDescription] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [price, setPrice] = useState([]);
+  const [offerPrice, setOfferPrice] = useState([]);
+
   return (
     <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
       <form className="md:p-10 p-4 space-y-5 max-w-lg">
