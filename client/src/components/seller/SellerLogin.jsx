22abe5c8b51434788bd311f59aca8977efd22f1f
Vedant <vedant28j@gmail.com>
2025-08-16 00:15:22 +0000

    error corrected : wrong destructing of useAppContext states

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 
 const SellerLogin = () => {
-  const [isSeller, setIsSeller, navigate] = useAppContext;
+  const { isSeller, setIsSeller, navigate } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
