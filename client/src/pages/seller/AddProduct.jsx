c7f0b1a3d42eb834ee20530c6141e16902fbe297
Vedant <vedant28j@gmail.com>
2025-08-16 02:53:49 +0000

    changing color to primary

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -130,7 +130,7 @@ const AddProduct = () => {
             />
           </div>
         </div>
-        <button className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded">
+        <button className="px-8 py-2.5 bg-primary text-white font-medium rounded">
           ADD
         </button>
       </form>
