8c2d8b3aa0b2d3d5c07f59e40cfeee071f14ff01
Vedant <vedant28j@gmail.com>
2025-08-16 00:33:42 +0000

    adding greencart logo to the top of the page

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { useAppContext } from "../../context/AppContext";
 
@@ -16,14 +17,14 @@ const SellerLayout = () => {
 
   return (
     <>
-      <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
-        <a href="https://prebuiltui.com">
+      <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
+        <Link to="/">
           <img
-            className="h-9"
-            src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg"
-            alt="dummyLogoColored"
+            src={assets.logo}
+            alt="logo"
+            className="cursor-pointer w-34 md:w-38"
           />
-        </a>
+        </Link>
         <div className="flex items-center gap-5 text-gray-500">
           <p>Hi! Admin</p>
           <button className="border rounded-full text-sm px-4 py-1">
