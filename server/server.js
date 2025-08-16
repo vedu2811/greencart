b034040bbd9cb3e66ec8654977c1a756e728404f
Vedant <vedant28j@gmail.com>
2025-08-16 20:55:24 +0000

    importing the connectDB function from db.js file, thus connected to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
 
 const app = express();
 const port = process.env.PORT || 2811;
+
+await connectDB();
 
 // ALLOW MULTIPLE ORIGINS
 const allowedOrigins = ["http://127.0.0.1:5173"];
 
 // MIDDLEWARE CONFIGURATIONS
-app.unsubscribe(express.json());
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
