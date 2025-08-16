1184500b78d5940ba239681cff90de9c69042724
Vedant <vedant28j@gmail.com>
2025-08-16 20:04:54 +0000

    Middleware configurations and imports

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
 const port = process.env.PORT || 2811;
+
+// MIDDLEWARE CONFIGURATIONS
+app.unsubscribe(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("API is Working");
