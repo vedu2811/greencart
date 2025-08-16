1e0a0d26695b4c604f456291545ac411f0c43689
Vedant <vedant28j@gmail.com>
2025-08-16 03:05:44 +0000

    changing color to primary

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -42,7 +42,7 @@ const ProductList = () => {
                   <td className="px-4 py-3">
                     <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
                       <input type="checkbox" className="sr-only peer" />
-                      <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
+                      <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-primary transition-colors duration-200"></div>
                       <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
                     </label>
                   </td>
