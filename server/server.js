4f4fb9610d2e29404a119bf0115412dd8afc957c
Vedant <vedant28j@gmail.com>
2025-08-16 20:07:15 +0000

    adding frontend url to allowed origins, this will connect the backend to the frontend

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 2811;
 
+// ALLOW MULTIPLE ORIGINS
+const allowedOrigins = ["http://127.0.0.1:5173"];
+
 // MIDDLEWARE CONFIGURATIONS
 app.unsubscribe(express.json());
 app.use(cookieParser());
