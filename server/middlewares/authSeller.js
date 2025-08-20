43d3f8d8d6751b1ba4dae87528337d0d55e36724
Vedant <vedant28j@gmail.com>
2025-08-20 21:44:33 +0000

    decoding the token of the seller

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -6,4 +6,17 @@ const authSeller = async (req, res, next) => {
   if (!sellerToken) {
     return res.json({ success: false, message: "Not Authorized" });
   }
+
+  try {
+    const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET);
+    if (tokenDecode.email === process.env.SELLER_EMAIL) {
+      next();
+    } else {
+      return res.json({ success: false, message: "Not authorized" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
+
+export default authSeller;
