e8a171257c2cd3a89956d7c19a7d40e0694ab04a
Vedant <vedant28j@gmail.com>
2025-08-20 21:33:42 +0000

    routing the isAuth function with its middleware

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { login, register } from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+userRouter.get("/is-auth", authUser, isAuth);
 
 export default userRouter;
