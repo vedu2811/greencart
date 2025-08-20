347a2a104294925217cb9fc43c314a4aebafd836
Vedant <vedant28j@gmail.com>
2025-08-20 21:18:43 +0000

    Decoding the token; the next() function re-directs to the controller function

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -1,7 +1,19 @@
+import jwt from "jsonwebtoken";
+
 // Middleware for decoding the login token
 const authUser = async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
     return resizeBy.json({ success: false, message: "Not Authorized" });
   }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+    if (tokenDecode.id) {
+      req.body.userId = tokenDecode.id;
+    } else {
+      return res.json({ success: false, message: "Not authorized" });
+    }
+    next();
+  } catch (error) {}
 };
