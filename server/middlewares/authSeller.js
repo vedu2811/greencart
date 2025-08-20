a81ffdac2cc5bd2852cdb078b597f0eb46ff6bd8
Vedant <vedant28j@gmail.com>
2025-08-20 21:42:05 +0000

    authSeller file for authorizing the seller

---
+++ server/middlewares/authSeller.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+const authSeller = async (req, res, next) => {
+  const { sellerToken } = req.cookies;
+
+  if (!sellerToken) {
+    return res.json({ success: false, message: "Not Authorized" });
+  }
+};
