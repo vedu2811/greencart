b41222b2405520c701a4a5c77d5e9dfd9e821819
Vedant <vedant28j@gmail.com>
2025-08-20 21:32:27 +0000

    Logout function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -102,3 +102,20 @@ export const isAuth = async (req, res) => {
     res.json({ success: false, messgae: error.message });
   }
 };
+
+///////////////////////////////////////////////////////////////////////
+// Logout User : /api/user/logout
+///////////////////////////////////////////////////////////////////////
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    res.json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
