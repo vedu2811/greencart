76e744d09abc5765cc3b3df82a889212463473c9
Vedant <vedant28j@gmail.com>
2025-08-20 22:27:01 +0000

    seller authentication function

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -30,3 +30,15 @@ export const sellerLogin = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+///////////////////////////////////////////////////////////////////////
+// Seller Authentication : /api/seller/is-auth
+///////////////////////////////////////////////////////////////////////
+export const isSellerAuth = async (req, res) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, messgae: error.message });
+  }
+};
