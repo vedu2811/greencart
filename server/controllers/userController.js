b91c544ced22bd6ab9081f453350ab982f057fd2
Vedant <vedant28j@gmail.com>
2025-08-20 20:30:48 +0000

    Catch block

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,6 +38,7 @@ export const register = async (req, res) => {
       user: { email: user.email, name: user.name },
     });
   } catch (error) {
+    console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 };
@@ -82,5 +83,8 @@ export const login = async (req, res) => {
       success: true,
       user: { email: user.email, name: user.name },
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
