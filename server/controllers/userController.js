6a67ebddb2287ac90d3946b0a76aef5dd15882d6
Vedant <vedant28j@gmail.com>
2025-08-20 16:14:37 +0000

    Creating the user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,5 +16,6 @@ export const register = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
   } catch (error) {}
 };
