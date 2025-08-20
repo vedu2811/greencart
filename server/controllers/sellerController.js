7a397d02fc9b7d22397c6e23da5e0f39d0cc0df2
Vedant <vedant28j@gmail.com>
2025-08-20 22:21:29 +0000

    adding try catch block to avoid error

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -4,22 +4,29 @@ import jwt from "jsonwebtoken";
 // Seller Login : /api/seller/login
 ///////////////////////////////////////////////////////////////////////
 export const sellerLogin = async (req, res) => {
-  const { email, password } = req.body;
-  if (
-    password === process.env.SELLER_PASSWORD &&
-    email === process.env.SELLER_EMAIL
-  ) {
-    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
+  try {
+    const { email, password } = req.body;
+    if (
+      password === process.env.SELLER_PASSWORD &&
+      email === process.env.SELLER_EMAIL
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
 
-    res.cookie("sellerToken", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+      res.cookie("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
 
-    return res.json({ success: true, message: "Logged In" });
+      return res.json({ success: true, message: "Logged In" });
+    } else {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
   }
 };
