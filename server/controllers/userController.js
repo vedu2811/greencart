5f7089da78034f0952e48beea2b26f56bf19c2ad
Vedant <vedant28j@gmail.com>
2025-08-20 15:26:18 +0000

    user Controller file for registering new users

---
+++ server/controllers/userController.js
@@ -0,0 +1,10 @@
+// Register User : /api/user/register
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+  } catch (error) {}
+};
