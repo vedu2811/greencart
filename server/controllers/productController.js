58ea1d6b325cfe5917924ae21bb3b1b4e61f4083
Vedant <vedant28j@gmail.com>
2025-08-20 22:37:56 +0000

    product controller functions to be used

---
+++ server/controllers/productController.js
@@ -0,0 +1,11 @@
+// Add Product : /api/product/add
+export const addProduct = async (req, res) => {};
+
+// Get Product : /api/product/list
+export const productList = async (req, res) => {};
+
+// Get Single Product : /api/product/id
+export const productById = async (req, res) => {};
+
+// Change Product inStock : /api/product/stock
+export const changeStock = async (req, res) => {};
