26c4e58c82e2155bcffd6db49debbd0b2ad558a6
Vedant <vedant28j@gmail.com>
2025-08-21 00:59:08 +0000

    Updating the stock for any single product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -38,7 +38,7 @@ export const productList = async (req, res) => {
 // Get Single Product : /api/product/id
 export const productById = async (req, res) => {
   try {
-    const id = req.body;
+    const { id } = req.body;
     const product = await Product.findById(id);
     res.json({ success: true, product });
   } catch (error) {
@@ -48,4 +48,13 @@ export const productById = async (req, res) => {
 };
 
 // Change Product inStock : /api/product/stock
-export const changeStock = async (req, res) => {};
+export const changeStock = async (req, res) => {
+  try {
+    const { id, inStock } = req.body;
+    await Product.findByIdAndUpdate(id, { inStock });
+    res.json({ success: true, message: "Stock Updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
