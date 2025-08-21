f8296c0f30fbc222d8ce3ea2b687d7f00e2b891f
Vedant <vedant28j@gmail.com>
2025-08-21 01:03:25 +0000

    exporting the product router

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -14,3 +14,5 @@ productRouter.post("/add", upload.array([images]), authSeller, addProduct);
 productRouter.get("/list", productList);
 productRouter.get("/id", productById);
 productRouter.post("/stock", authSeller, changeStock);
+
+export default productRouter;
