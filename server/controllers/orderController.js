f0bafe913e09ab75dcbc46c64e50b1a0e8455264
Vedant <vedant28j@gmail.com>
2025-08-21 01:48:22 +0000

    getting the total amount of the cart

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -7,5 +7,12 @@ export const placeOrderCOD = async (req, res) => {
     if (!address || items.length === 0) {
       return res.json({ success: true, message: "Invalid Data" });
     }
+    // Calculate amount using items
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+    // Add tax (2%)
+    amount += Math.float(amount * 0.02);
   } catch (error) {}
 };
