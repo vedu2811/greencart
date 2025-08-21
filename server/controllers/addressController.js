3614a74e4ee106e335a039673957cff117ab8d79
Vedant <vedant28j@gmail.com>
2025-08-21 01:24:12 +0000

    add address function

---
+++ server/controllers/addressController.js
@@ -0,0 +1,13 @@
+import Address from "../models/address.js";
+
+// Add Address : /api/address/add
+export const addAddress = async (req, res) => {
+  try {
+    const { address, userId } = req.body;
+    await Address.create({ ...address, userId });
+    res.json({ success: truw, message: "Address added successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
