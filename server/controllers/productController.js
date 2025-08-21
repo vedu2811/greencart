38731c9a75a32f225c7405ffaf060de63e15fb75
Vedant <vedant28j@gmail.com>
2025-08-21 00:56:44 +0000

    Get single product function : using product id for extraction

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -36,7 +36,16 @@ export const productList = async (req, res) => {
 };
 
 // Get Single Product : /api/product/id
-export const productById = async (req, res) => {};
+export const productById = async (req, res) => {
+  try {
+    const id = req.body;
+    const product = await Product.findById(id);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Change Product inStock : /api/product/stock
 export const changeStock = async (req, res) => {};
