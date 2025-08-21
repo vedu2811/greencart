7ea6f1c1a89833625b40c237cd45fa6e53cda16e
Vedant <vedant28j@gmail.com>
2025-08-21 00:25:51 +0000

    Complete addProduct function - taking the descriptive data from the product model and then uploading image to cloudinary, extracting its secure url and adding all the product data into a single array

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import Product from "../models/product";
+
 // Add Product : /api/product/add
-export const addProduct = async (req, res) => {};
+export const addProduct = async (req, res) => {
+  try {
+    let productData = JSON.parse(req.body.productData);
+    const images = req.files;
+
+    let imagesURL = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+    await Product.create({ ...productData, image: imagesURL });
+
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Get Product : /api/product/list
 export const productList = async (req, res) => {};
