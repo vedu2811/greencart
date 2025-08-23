2c9468f4b7c10f279882fe23b2da3a3ade351fad
Vedant <vedant28j@gmail.com>
2025-08-23 21:36:16 +0000

    adding seller login data through api

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 
 const SellerLogin = () => {
-  const { isSeller, setIsSeller, navigate } = useAppContext();
+  const { isSeller, setIsSeller, navigate, axios } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmitHandler = async (event) => {
-    event.preventDefault();
-    setIsSeller(true);
+    try {
+      event.preventDefault();
+      const { data } = await axios.post("/api/seller/login", {
+        email,
+        password,
+      });
+    } catch (error) {}
   };
 
   useEffect(() => {
