44877d526ebd49dbbc7695a37552d30f7f9d469a
Vedant <vedant28j@gmail.com>
2025-08-23 20:56:44 +0000

    Adding all the routes to the order router

---
+++ server/routes/orderRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authUser from "../middlewares/authUser.js";
+import {
+  getUserOrders,
+  placeOrderCOD,
+  getAllOrders,
+} from "../controllers/orderController";
+import authSeller from "../middlewares/authSeller.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/cod", authUser, placeOrderCOD);
+orderRouter.get("/user", authUser, getUserOrders);
+orderRouter.get("/seller", authSeller, getAllOrders);
+
+export default orderRouter;
