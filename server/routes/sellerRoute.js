d5d3b7b961a59dcb266186362431da8f2bf91ddf
Vedant <vedant28j@gmail.com>
2025-08-23 21:58:06 +0000

    error corrected

--- server/routes/sellerRoute.js
+++ server/routes/sellerRoute.js
@@ -9,7 +9,7 @@ import authSeller from "../middlewares/authSeller.js";
 const sellerRouter = express.Router();
 
 sellerRouter.post("/login", sellerLogin);
-sellerRouter.post("/is-auth", authSeller, isSellerAuth);
-sellerRouter.post("/logout", sellerLogout);
+sellerRouter.get("/is-auth", authSeller, isSellerAuth);
+sellerRouter.get("/logout", sellerLogout);
 
 export default sellerRouter;
