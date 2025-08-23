e3a36b9a93e88065f8bde55d629351b341875506
Vedant <vedant28j@gmail.com>
2025-08-23 21:02:53 +0000

    error corrected

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import Product from "../models/product";
+import Product from "../models/product.js";
 
 // Add Product : /api/product/add
 export const addProduct = async (req, res) => {
