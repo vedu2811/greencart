06c56d326756321ec464d083de70fe0b90a076d1
Vedant <vedant28j@gmail.com>
2025-08-24 01:39:08 +0000

    fetch products function: removing dummy data and adding dynamic data

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyProducts } from "../assets/assets";
+// import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
 import axios from "axios";
 
@@ -39,7 +39,16 @@ export const AppContextProvider = ({ children }) => {
 
   // Fetch All Products
   const fetchProducts = async () => {
-    setProducts(dummyProducts);
+    try {
+      const { data } = await axios.get("/api/product/list");
+      if (data.success) {
+        setProducts(data.products);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   // Add Product to Cart
@@ -120,6 +129,7 @@ export const AppContextProvider = ({ children }) => {
     searchQuery,
     setSearchQuery,
     axios,
+    fetchProducts,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
