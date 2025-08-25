c9f328d8af30db96e58a0cc1a585cc593e4e5f84
Vedant <vedant28j@gmail.com>
2025-08-25 21:30:44 +0000

    importing user model

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,7 @@
 import Order from "../models/order.js";
 import Product from "../models/product.js";
 import stripe from "stripe";
+import User from "../models/user.js";
 
 // Placing Order COD : /api/orde/cod
 export const placeOrderCOD = async (req, res) => {
