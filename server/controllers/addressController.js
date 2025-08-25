632f1c3fae248b88af80c67a894e71fb3893dea7
Vedant <vedant28j@gmail.com>
2025-08-25 01:22:52 +0000

    error corrected

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -5,7 +5,7 @@ export const addAddress = async (req, res) => {
   try {
     const { address, userId } = req.body;
     await Address.create({ ...address, userId });
-    res.json({ success: truw, message: "Address added successfully" });
+    res.json({ success: true, message: "Address added successfully" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
@@ -17,7 +17,7 @@ export const getAddress = async (req, res) => {
   try {
     const { userId } = req.body;
     const addresses = await Address.find({ userId });
-    res.json({ success: truw, addresses });
+    res.json({ success: true, addresses });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
