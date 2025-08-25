f461d76f91c2d817a35454fefa6079650d60941d
Vedant <vedant28j@gmail.com>
2025-08-25 04:55:06 +0000

    connecting api: place order with stripe function

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -73,6 +73,21 @@ const Cart = () => {
         } else {
           toast.error(data.message);
         }
+      } else {
+        // Place Order using Stripe
+        const { data } = await axios.post("/api/order/stripe", {
+          userId: user._id,
+          items: cartArray.map((item) => ({
+            product: item._id,
+            quantity: item.quantity,
+          })),
+          address: selectedAddress._id,
+        });
+        if (data.success) {
+          window.location.replace(data.url);
+        } else {
+          toast.error(data.message);
+        }
       }
     } catch (error) {
       toast.error(error.message);
