fb5731fdcdc9f1020a047ddba59fdfab077f0159
Vedant <vedant28j@gmail.com>
2025-08-25 16:26:21 +0000

    new loading file that shows the loader spin while waiting for stripe to complete its payment

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary"></div>
+    </div>
+  );
+};
+
+export default Loading;
